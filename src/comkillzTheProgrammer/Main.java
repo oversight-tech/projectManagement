6e86f251e1567d85313d43e63c1ddc64660a4583
skillzTheProgrammer <skillztheprogrammer@gmail.com>
2020-02-12 15:14:03 +0100

    Update Main.java
    skillz edit

--- src/comkillzTheProgrammer/Main.java
+++ src/comkillzTheProgrammer/Main.java
@@ -1,10 +1,19 @@
 package comkillzTheProgrammer;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
+
+    static Function<Integer,IntStream> data =t-> IntStream.range(0,t).filter(num->num%2==0);
 
     public static void main(String[] args) {
 	// write your code here
-        EvenClass evenClass = new EvenClass();
-        evenClass.evenPrint(100);
+        IntStream list = data.apply(30);
+        list.forEach(System.out::println);
+
     }
 }
